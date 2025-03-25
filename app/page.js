f5764f50cb93774c95759970bab8d53c0344b635
finesse-mumbai <web@finesse.co.in>
2025-03-25 14:18:07 +0530

    aiftp first commit

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <PreHeader />
       <Header />
       <ImageSlider />
-      <div className="grid grid-cols-1 md:grid-cols-[40%_60%] gap-4  px-2 md:px-16 mt-3 md:mt-16">
+      <div className="grid grid-cols-1 md:grid-cols-[40%_60%] md:gap-4 gap-8  px-2 md:px-16 mt-8 md:mt-16">
         <PresidentPage />
         <ArticleList />
       </div>
