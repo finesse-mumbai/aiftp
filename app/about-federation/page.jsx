cf86d3135c4e5c9cab0260daa10f703a146ea678
TejasviVichare <tejasvivichare2121@gmail.com>
2025-03-28 12:35:17 +0530

    About Page Added

---
+++ app/about-federation/page.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import Heading from "../components/Heading/Heading";
+
+const page = () => {
+  return (
+    <div className='mx-auto md:px-20 pt-12 px-1 p-5"'>
+      <h1 className="text-center font-bold md:text-left">
+        <Heading headingText="About Federation" />
+      </h1>
+     <div className="py-6">
+        <hr />
+    </div>
+      <div className="text-justify text-[#868686]">
+        <p>
+          Inspired by the ideology to have a common platform for all those who
+          practice taxation laws, irrespective of their individual affiliations
+          and to enable them to share the benefits of their learning and sharing
+          of knowledge, eminent professionals from the fields of Direct and
+          Indirect Taxes conceived the idea of establishing an All-India body
+          for the tax practitioners. It was at the opening ceremony of the
+          National Conference held on November 11, 1976 organised by The Chamber
+          of Income Tax Consultants, Mumbai under Presidentship of Shri B. C.
+          Joshi, that the doyens of the Professionals christened the Association
+          in the presence of former Chief Justice of India, Hon&rsquo;ble
+          Justice J. C. Shah, distinguished Jurist Padma Vibhushan Dr. N. A.
+          Palkhivala Senior Advocate and Shri Ram Rao Adik, Senior Advocate,
+          Advocate General of Maharashtra. Shri N. C. Mehta, Chartered
+          Accountant, Mumbai, was elected as Founder President of the All-India
+          Federation of Tax Practitioners (<strong>AIFTP</strong>) and Shri P.
+          C. Joshi Advocate was elected as Secretary General. The AIFTP has
+          completed 48 glorious years of its existence.
+        </p>
+        <p className="mt-3">
+          The main object of AIFTP is to spread education in the matters
+          relating to tax laws, other laws and Accountancy.
+        </p>
+        <p className="mt-3">
+          The AIFTP has its registered Head Office at 215, Rewa Chambers, 31,
+          New Marine Lines, Mumbai- 400 020. The total strength of National
+          Executive Committee Members is 75 headed by the National President,
+          Deputy President, five Vice-Presidents, Secretary General, Treasurer,
+          five Joint Secretaries. The National President (2025) is Mr. Samir S.
+          Jani, Advocate from Junagadh and Deputy President CA. S. Venkataramani
+          from Bengaluru.
+        </p>
+        <p className="mt-3">
+          The AIFTP is divided into five zones namely, Central Zone, Eastern
+          Zone, Northern Zone, Southern Zone and Western Zone. Each Zone is
+          headed by a Chairman, and Vice Chairman from each State to which Zone
+          represents, One Secretary and two Joint Secretaries and Treasurer as
+          Office Bearers. The term of the Zonal Office bearers and Managing
+          Committee coincide with the terms of the National Executive Committee.
+        </p>
+        <p className="mt-3">
+          The membership of the AIFTP comprise of Senior Advocates, Advocates,
+          Solicitors, Chartered Accountants, Company Secretaries, Cost
+          Accountants and Tax Practitioners who are practicing on Direct &amp;
+          Indirect Taxes, from all States and union territory of the Country.
+          Members of the AIFTP enjoy a strong bond of fellowship leading to
+          fraternal brotherhood amongst professionals. The AIFTP is the only
+          voluntary professional organisation of our country which has 146
+          Professional Associations as its affiliated members and more than
+          11,000 individuals as life members from 29 States and 4 Union
+          Territories.
+        </p>
+        <p className="mt-3">
+          For spreading the educational activities, the AIFTP has various
+          Sub-Committees, such as, Journal Committee, Law &amp; Representation
+          Committee (Direct &amp; Indirect Taxes), ITAT Bar Associations&rsquo;
+          Co-ordination Committee, Membership Development, Times Committee,
+          Publication Committee, etc.
+        </p>
+        <p className="mt-3">
+          The AIFTP publishes a monthly Journal covering the latest reported and
+          unreported decisions of the Supreme Court, High Courts and Income Tax
+          Appellate Tribunals, allied laws, and also the articles, opinions and
+          Q&amp;A. The unique feature of the AIFTP Journal is that every
+          quarter, the gist of important case Laws which are published in 33 Tax
+          Magazines,&nbsp;
+          <a href="http://www.itatonline.org/" className="no-underline font-bold hover:underline">www.itatonline.org</a>&nbsp;are
+          published section wise. Yearly digest of case laws from 2003 onwards
+          are also available on the website i.e.,&nbsp;
+          <a href="http://www.aiftponline.org/" className="no-underline font-bold hover:underline">www.aiftponline.org</a>
+          &nbsp;which can be downloaded by the members and tax professionals.
+          From 2019 onwards, AIFTP also publishes a monthly Journal on Indirect
+          Tax &amp; Corporate Laws Journal.
+        </p>
+        <p className="mt-3">
+          AIFTP publishes a monthly newsletter called AIFTP TIMES which is sent
+          / emailed to all the members free of charge. Newsletter contains
+          important Notifications, Circulars and other topical information and
+          also information about various activities of the AIFTP.
+        </p>
+        <p className="mt-3">
+          AIFTP website i.e.,&nbsp;
+              <a href="http://www.aiftponline.org/" className="no-underline font-bold hover:underline">www.aiftponline.org</a>
+          , have been revamped in 2023 and is an informative source for the
+          members. The website is regularly updated by a team of dedicated
+          professionals. The Journals and Times are uploaded every month on the
+          website of AIFTP. Professionals can apply Membership, Journal
+          Subscriptions, ID card through website.
+        </p>
+        <p className="mt-3">
+          AIFTP has been making representations for better tax law and tax
+          administration. AIFTP and Associate members have filed more than 37
+          Public Interest Petitions (<strong>PILs</strong>) before various
+          Courts for better administration of tax laws and to uphold the
+          independency of judicial forums. AIFTP regularly sends Pre- and
+          Post-Budget Memorandums. Many of the suggestions and the
+          recommendations are accepted. It regularly publishes books in simple
+          language and question-answer format at a low cost. It has published
+          more than 48 publications till date.
+        </p>
+        <p className="mt-3">
+          AIFTP jointly with the Association Members, organise National
+          Seminars, Conferences and Conventions in various parts of the Country
+          to update its members on Direct and Indirect Taxation. A unique
+          feature of the AIFTP is that its faculties, chairmen, trustees, office
+          bearers and members of the National Executive and Zonal Committees pay
+          a registration fee and bear their own travel and stay expenses. From
+          April 2020 to September 2021 during the period of Covid-19 Pandemic,
+          the AIFTP has conducted more than 200 Webinars on various subjects and
+          all webinars were without any charges. AIFTP as an association with
+          the help of their members have contributed an amount of Rs. 11 lakhs
+          to the Prime Minister Cares Fund. In the year, 1999 the AIFTP had
+          published a publication titled &ldquo;
+          <strong>NRI &ndash; A Legal companion&rdquo;</strong>&nbsp;which was
+          dedicated to the War Heroes of Kargil and the entire surplus of the
+          said publication was handed over to the Defence fund. AIFTP members
+          have sponsored various awards in memories of their respective parents
+          in various categories i.e., Best Conference, Best Zone, Best Chairman,
+          Best Upcoming Speakers, etc.
+        </p>
+        <p className="mt-3">
+          Since 2004, AIFTP conducts regular International Study tours, in the
+          first study tour a seminar was held at Law Society of England and
+          Wales on April 24, 2004, wherein a publication titled &ldquo;
+          <strong>India-A Global Business Destination</strong>&rdquo; was
+          released at England.
+        </p>
+        <p className="mt-3">
+          AIFTP has voluntarily adopted a Code of Ethics for its members in its
+          Constitution.
+        </p>
+        <p className="mt-3">
+          On request of the AIFTP the Government of India has released
+          Commemorative Postage Stamp in Memory of Padma Vibhushan Late Dr. N.
+          A. Palkhivala, Senior Advocate on 16th January, 2004. The then
+          Hon&rsquo;ble Prime Minister of India, Shri Atal Bihari Vajpayee
+          released the Commemorative Postage Stamp at Mumbai.
+        </p>
+        <p className="mt-3">
+          Many members of the AIFTP have been elevated as Judges of the Supreme
+          Court, High Courts, Tribunals and also appointed as Advocate General
+          of various States. &nbsp;
+        </p>
+        <p className="mt-3">
+          The AIFTP has always believed in professional brotherhood. For support
+          of the members of the AIFTP it has set aside a corpus of Rs 1 crores
+          for welfare of the members of the AIFTP. During the pandemic the AIFTP
+          has supported financial aides many deserving members from across the
+          country. The AIFTP is also proposing to have a group insurance scheme
+          for the benefit of its members.&nbsp;
+        </p>
+        <p className="mt-3">
+          The AIFTP is the symbol and spirit of National Integration of tax
+          professionals, and with active support of the members it is one of the
+          leading National Tax professional organisations.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
